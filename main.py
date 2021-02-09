f866077189789cc4c8c1c492b072f799c9c9a946
Ana Carolina Santos <54289776+carolinasrc@users.noreply.github.com>
2021-02-09 15:08:38 -0300

    voila

---
+++ main.py
@@ -0,0 +1,28 @@
+import tweepy, argparse, os
+
+# variáveis de ambiente de autentificação 
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+
+# Inicializando o parse
+parser = argparse.ArgumentParser(
+description="simple script to demonstrate argparse usage"
+)
+
+# Adicionando um parâmetro posicional 
+parser.add_argument('printme', help="The string tomain.py be printed")
+
+# Fazendo parse dos argumentos
+arguments = parser.parse_args()
+
+# Imprimindo a String 
+print(arguments.printme)
+
+# Acessando, enviando e recebendo o response da mensagem
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+response = api.update_status(arguments.printme)
+print('Status posted', response)
