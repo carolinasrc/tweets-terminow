8038369fe15d014670269e40059ea269ecb9e29a
Carolina Soares <54289776+carolinasrc@users.noreply.github.com>
2021-02-09 15:15:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ arguments = parser.parse_args()
 # Imprimindo a String 
 print(arguments.printme)
 
-# Acessando, enviando e recebendo o response da mensagem
+# Acessando, enviando e recebendo a response da mensagem
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
